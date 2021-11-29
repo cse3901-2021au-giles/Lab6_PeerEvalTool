c5ad331c45a0a40989b93107ce2d93367f2545fc
Mesharp01 <sharp.475@osu.edu>
2021-11-28 20:56:33 -0500

    added controller for evaluations with CRUD capabilities. added CRUD to instructor controller as well.

---
+++ app/controllers/evals_controller.rb
@@ -0,0 +1,47 @@
+class EvalsController < ApplicationController
+    before_action :current_eval, only: [:show, :edit, :update, :destroy]
+    
+      def index
+        @evals = Eval.all
+      end
+    
+      def show
+    
+      end
+    
+      def new
+        @eval = Eval.new
+      end
+      
+      def create
+        eval = Eval.create(eval_params)
+        
+        redirect_to evals_path
+      end
+    
+      def edit
+    
+      end
+      
+      def update
+        @eval.update(eval_params)
+        
+        redirect_to eval_path(@eval)
+      end
+      
+      def destroy
+        @eval.destroy
+    
+        redirect_to evals_path
+      end
+      
+      private
+      
+      def eval_params
+        params.require(:eval).permit(:comments, :ratings)
+      end
+      
+      def current_eval
+        @eval = Eval.find(params[:id])
+      end
+    end
---
+++ test/controllers/evaluations_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class EvaluationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get evaluations_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get evaluations_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get evaluations_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get evaluations_edit_url
+    assert_response :success
+  end
+
+end
