8a479be28c623a8165aa328b39a3401b93fa4365
Mesharp01 <sharp.475@osu.edu>
2021-11-30 20:48:59 -0500

    fixed all users page so there is a list of users at /users and the ability to add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -11,12 +16,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "Welcome"
-      redirect_to @user
-      # Handle a successful save.
+      redirect_to users_path
     else
       render 'new'
     end
+  end
+
+  def update
+    @user.update(user_params)
+    flash[:success] = "Profile Updated"
+    
+    redirect_to users_path(@user)
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "User Deleted"
+
+    redirect_to users_path
   end
 
   private
@@ -24,6 +41,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def current_user
+      user = User.find(params[:id])
     end
 
 end
