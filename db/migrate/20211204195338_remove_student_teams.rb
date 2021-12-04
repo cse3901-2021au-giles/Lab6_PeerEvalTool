c5f6305562e7c2324b6f4603c178bf96a09152a6
Taylor Keckley <keckley.17@osu.edu>
2021-12-04 18:57:34 -0500

    Removed extra db migration and ran webpacker:install again.

--- db/migrate/20211204195338_remove_student_teams.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStudentTeams < ActiveRecord::Migration[6.0]
-  def change
-      drop_table :Students_Teams
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_12_03_021721) do
-=======
 ActiveRecord::Schema.define(version: 2021_12_04_195839) do
->>>>>>> Teams
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -53,10 +49,6 @@ ActiveRecord::Schema.define(version: 2021_12_04_195839) do
     t.integer "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
-    t.integer "team_id"
-=======
->>>>>>> Teams
     t.index ["course_id"], name: "index_students_on_course_id"
     t.index ["email"], name: "index_students_on_email", unique: true
   end
@@ -66,14 +58,11 @@ ActiveRecord::Schema.define(version: 2021_12_04_195839) do
     t.integer "team_id"
   end
 
-<<<<<<< HEAD
-=======
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
->>>>>>> Teams
   add_foreign_key "students", "courses"
 end
