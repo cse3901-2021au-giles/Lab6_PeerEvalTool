3ab53a7e68042aa6babd2ce52dbe05f9a39371ce
Alex Merarana <azmerana03@gmail.com>
2021-12-07 17:46:44 -0500

    Stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_050913) do
+ActiveRecord::Schema.define(version: 2021_12_07_214449) do
 
   create_table "courses", force: :cascade do |t|
     t.string "cname"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_050913) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
