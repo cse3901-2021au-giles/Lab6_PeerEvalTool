81d41b045ff3d53b5bf67adf3d2c5d78b5313b19
Taylor Keckley <keckley.17@osu.edu>
2021-12-07 10:45:43 -0500

    Fixed merge conflicts.

--- app/models/evaluate.rb
+++ app/models/evaluate.rb
@@ -1,13 +1,4 @@
 class Evaluate < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :user
-    belongs_to :group
-    has_many :ratings
-      
-    validates :user_id, presence: true
-    validates :ratee_id, presence: true
-    validates :group_id, presence: true
-=======
   belongs_to :user
   belongs_to :group
   has_many :ratings
@@ -15,5 +6,4 @@ class Evaluate < ApplicationRecord
   validates :user_id, presence: true
   validates :ratee_id, presence: true
   validates :group_id, presence: true
->>>>>>> 43d7c4a333ba1e5f2c3782b723a3a2c8dd3c64e6
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,17 +1,8 @@
 class Rating < ApplicationRecord
-<<<<<<< HEAD
     belongs_to :user
     belongs_to :evaluate
     
     validates :evaluate_id, presence: true
     validates :user_id, presence: true
     validates :group_id, presence: true
-=======
-  belongs_to :user
-  belongs_to :evaluate
-  
-  validates :evaluate_id, presence: true
-  validates :user_id, presence: true
-  validates :group_id, presence: true
->>>>>>> 43d7c4a333ba1e5f2c3782b723a3a2c8dd3c64e6
 end
