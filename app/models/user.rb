852a4702f243b1dafba618896d63adb4ffd8cbd1
Mesharp01 <sharp.475@osu.edu>
2021-12-07 10:50:46 -0500

    fixed projects table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,6 @@ class User < ApplicationRecord
   has_many :courses
   has_many :evaluates
 
-<<<<<<< HEAD
-=======
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
->>>>>>> 43d7c4a333ba1e5f2c3782b723a3a2c8dd3c64e6
   before_save { email.downcase! }
   validates :Fname, presence: true, length: { maximum: 50 }
   validates :Lname, presence: true, length: { maximum: 50 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_141539) do
+ActiveRecord::Schema.define(version: 2021_12_07_154945) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "group_id"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2021_12_07_141539) do
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
