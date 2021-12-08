f38d85b3a3b175480e62d203fcc149f9c793e92e
Alex Merarana <azmerana03@gmail.com>
2021-12-07 21:49:37 -0500

    Fixed sizing issues with tables and other things

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    instructors = User.create([{Fname: "instructor", Lname: "1", email: "instructor1@osu.edu", password: "qwerty", admin: true}])
+
+    students = User.create([
+        {Fname: "student", Lname: "1", email: "student1@osu.edu", password: "qwerty", admin: false},
+        {Fname: "student", Lname: "2", email: "student2@osu.edu", password: "qwerty", admin: false},
+        {Fname: "student", Lname: "3", email: "student3@osu.edu", password: "qwerty", admin: false},
+        {Fname: "student", Lname: "4", email: "student4@osu.edu", password: "qwerty", admin: false}])
+
+    courses = Course.create([{cname: "3901", user_id: 1},
+                                {cname: "3902", user_id: 1},
+                                {cname: "3903", user_id: 1}])
